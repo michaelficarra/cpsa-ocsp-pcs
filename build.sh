ece0632a6c34ad116fe06914f3491dda74e049aa
Michael Ficarra <git@michael.ficarra.me>
2011-05-02 15:55:15 -0400

    wrapped ttp responses in requesters' encryption keys and wrapped concatenation in resolution requests in requesters' signatures

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
-cpsa +RTS -M512m -RTS -o pcs.txt pcs.scm && \
+rm -f pcs.txt pcs.xhtml
+cpsa +RTS -M512m -RTS -o pcs.txt pcs.scm
 cpsagraph -o pcs.xhtml pcs.txt && \
-firefox -remote "openFile(`pwd`/pcs.xhtml)"
+google-chrome "`pwd`/pcs.xhtml"
--- pcs.scm
+++ pcs.scm
@@ -13,24 +13,24 @@
 	; (send (enc m b a t (privk "sign" b))) ; resp-sig-send
 
 	; ### aborter role
-	; (send (enc (enc m x y "abort" (privk x)) (pubk  t))) ; abort-request
-	; (recv (enc (enc m x y "abort" (privk x)) (privk t))) ; abort-response-success
-	; (recv (enc (enc m y x "abort" (privk y)) (privk t))) ; abort-response-fail-aborted
-	; (recv (enc m y x t (privk "sign" y)))                ; abort-response-fail-resolved
+	; (send (enc (enc m x y "abort" (privk x)) (pubk  t)))                                                ; abort-request
+	; (recv (enc (enc m x y "abort" (privk x)) (privk "sign" t)))                                         ; abort-response-success
+	; (recv (enc (enc (enc m y x "abort" (privk y)) (privk "sign" t)) (pubk "enc" x)))                    ; abort-response-fail-aborted
+	; (recv (enc (enc m y x t (privk "sign" y)) (pubk "enc" x)))                                          ; abort-response-fail-resolved
 
 	; ### resolver role
-	; (send (enc m y x t (privk "pcs" y)) (enc m x y t (privk "sign" x))) ; resolve-request
-	; (recv (enc m y x t (privk "sign" y)))                               ; resolve-response-success
-	; (recv (enc (enc m x y "abort" (privk x)) (privk t)))                ; resolve-response-fail
+	; (send (enc (cat (enc m y x t (privk "pcs" y)) (enc m x y t (privk "sign" x))) (privk "ttp-sig" x))) ; resolve-request
+	; (recv (enc (enc m y x t (privk "sign" y)) (pubk "enc" x)))                                          ; resolve-response-success
+	; (recv (enc (enc (enc m x y "abort" (privk x)) (privk "sign" t)) (pubk "enc" x)))                    ; resolve-response-aborted
 
 	; ### ttp role
-	; (recv (enc (enc m x y "abort" (privk x)) (pubk  t)))                ; ttp-abort-request
-	; (send (enc (enc m x y "abort" (privk x)) (privk t)))                ; ttp-abort-response-success
-	; (send (enc (enc m y x "abort" (privk y)) (privk t)))                ; ttp-abort-response-fail-aborted
-	; (send (enc m y x t (privk "sign" y)))                               ; ttp-abort-response-fail-resolved
-	; (recv (enc m y x t (privk "pcs" y)) (enc m x y t (privk "sign" x))) ; ttp-resolve-request
-	; (send (enc m y x t (privk "sign" y)))                               ; ttp-resolve-response-success
-	; (send (enc (enc m x y "abort" (privk x)) (privk t)))                ; ttp-resolve-response-aborted
+	; (recv (enc (enc m x y "abort" (privk x)) (pubk  t)))                                                ; ttp-abort-request
+	; (send (enc (enc (enc m x y "abort" (privk x)) (privk "sign" t)) (pubk "enc" x)))                    ; ttp-abort-response-success
+	; (send (enc (enc (enc m y x "abort" (privk y)) (privk "sign" t)) (pubk "enc" x)))                    ; ttp-abort-response-fail-aborted
+	; (send (enc (enc m y x t (privk "sign" y)) (pubk "enc" x)))                                          ; ttp-abort-response-fail-resolved
+	; (recv (enc (cat (enc m y x t (privk "pcs" y)) (enc m x y t (privk "sign" x))) (privk "ttp-sig" x))) ; ttp-resolve-request
+	; (send (enc (enc m y x t (privk "sign" y)) (pubk "enc" x)))                                          ; ttp-resolve-response-success
+	; (send (enc (enc (enc m x y "abort" (privk x)) (privk "sign" t)) (pubk "enc" x)))                    ; ttp-resolve-response-aborted
 
 
 	; ### init
@@ -50,7 +50,7 @@
 	; ### resp
 
 	; resp in main protocol
-	(defrole resp-000
+	(defrole resp-main
 		(vars (a b t name) (m text))
 		(trace
 			(recv (enc m a b t (privk "pcs" a)))  ; resp-pcs-recv
@@ -67,24 +67,24 @@
 	(defrole abort-success
 		(vars (x y t name) (m text))
 		(trace
-			(send (enc (enc m x y "abort" (privk x)) (pubk  t))) ; abort-request
-			(recv (enc (enc m x y "abort" (privk x)) (privk t))) ; abort-response-success
+			(send (enc (enc m x y "abort" (privk x)) (pubk  t)))        ; abort-request
+			(recv (enc (enc m x y "abort" (privk x)) (privk "sign" t))) ; abort-response-success
 		)
 	)
 	; unsuccessful abort because of previous abort
 	(defrole abort-fail-aborted
 		(vars (x y t name) (m text))
 		(trace
-			(send (enc (enc m x y "abort" (privk x)) (pubk  t))) ; abort-request
-			(recv (enc (enc m y x "abort" (privk y)) (privk t))) ; abort-response-fail-aborted
+			(send (enc (enc m x y "abort" (privk x)) (pubk  t)))                             ; abort-request
+			(recv (enc (enc (enc m y x "abort" (privk y)) (privk "sign" t)) (pubk "enc" x))) ; abort-response-fail-aborted
 		)
 	)
 	; unsuccessful abort because of previous resolution
 	(defrole abort-fail-resolved
 		(vars (x y t name) (m text))
 		(trace
-			(send (enc (enc m x y "abort" (privk x)) (pubk  t))) ; abort-request
-			(recv (enc m y x t (privk "sign" y)))                ; abort-response-fail-resolved
+			(send (enc (enc m x y "abort" (privk x)) (pubk  t)))       ; abort-request
+			(recv (enc (enc m y x t (privk "sign" y)) (pubk "enc" x))) ; abort-response-fail-resolved
 		)
 	)
 
@@ -95,16 +95,16 @@
 	(defrole resolve-success
 		(vars (x y t name) (m text))
 		(trace
-			(send (enc m y x t (privk "pcs" y)) (enc m x y t (privk "sign" x))) ; resolve-request
-			(recv (enc m y x t (privk "sign" y)))                               ; resolve-response-success
+			(send (enc (cat (enc m y x t (privk "pcs" y)) (enc m x y t (privk "sign" x))) (privk "ttp-sig" x))) ; resolve-request
+			(recv (enc (enc m y x t (privk "sign" y)) (pubk "enc" x)))                                          ; resolve-response-success
 		)
 	)
 	; unsuccessful resolution because of previous abort
 	(defrole resolve-fail
 		(vars (x y t name) (m text))
 		(trace
-			(send (enc m y x t (privk "pcs" y)) (enc m x y t (privk "sign" x))) ; resolve-request
-			(recv (enc (enc m x y "abort" (privk x)) (privk t)))                ; resolve-response-fail
+			(send (enc (cat (enc m y x t (privk "pcs" y)) (enc m x y t (privk "sign" x))) (privk "ttp-sig" x))) ; resolve-request
+			(recv (enc (enc (enc m x y "abort" (privk x)) (privk "sign" t)) (pubk "enc" x)))                    ; resolve-response-aborted
 		)
 	)
 
@@ -115,24 +115,24 @@
 	(defrole ttp-abort-success
 		(vars (x y t name) (m text))
 		(trace
-			(recv (enc (enc m x y "abort" (privk x)) (pubk  t))) ; ttp-abort-request
-			(send (enc (enc m x y "abort" (privk x)) (privk t))) ; ttp-abort-response-success
+			(recv (enc (enc m x y "abort" (privk x)) (pubk  t)))                             ; ttp-abort-request
+			(send (enc (enc (enc m x y "abort" (privk x)) (privk "sign" t)) (pubk "enc" x))) ; ttp-abort-response-success
 		)
 	)
 	; other party previously aborted; respond with cached response
 	(defrole ttp-abort-fail-aborted
 		(vars (x y t name) (m text))
 		(trace
-			(recv (enc (enc m x y "abort" (privk x)) (pubk  t))) ; ttp-abort-request
-			(send (enc (enc m y x "abort" (privk y)) (privk t))) ; ttp-abort-response-fail-aborted
+			(recv (enc (enc m x y "abort" (privk x)) (pubk  t)))                             ; ttp-abort-request
+			(send (enc (enc (enc m y x "abort" (privk y)) (privk "sign" t)) (pubk "enc" x))) ; ttp-abort-response-fail-aborted
 		)
 	)
 	; respond with signature from previous resolution
 	(defrole ttp-abort-fail-resolved
 		(vars (x y t name) (m text))
 		(trace
-			(recv (enc (enc m x y "abort" (privk x)) (pubk  t))) ; ttp-abort-request
-			(send (enc m y x t (privk "sign" y)))                ; ttp-abort-response-fail-resolved
+			(recv (enc (enc m x y "abort" (privk x)) (pubk  t)))       ; ttp-abort-request
+			(send (enc (enc m y x t (privk "sign" y)) (pubk "enc" x))) ; ttp-abort-response-fail-resolved
 		)
 	)
 
@@ -141,17 +141,32 @@
 	(defrole ttp-resolve-success
 		(vars (x y t name) (m text))
 		(trace
-			(recv (enc m y x t (privk "pcs" y)) (enc m x y t (privk "sign" x))) ; ttp-resolve-request
-			(send (enc m y x t (privk "sign" y)))                               ; ttp-resolve-response-success
+			(recv (enc (cat (enc m y x t (privk "pcs" y)) (enc m x y t (privk "sign" x))) (privk "ttp-sig" x))) ; ttp-resolve-request
+			(send (enc (enc m y x t (privk "sign" y)) (pubk "enc" x)))                                          ; ttp-resolve-response-success
 		)
 	)
 	; unsuccessful resolution due to previous abortion
 	(defrole ttp-resolve-aborted
 		(vars (x y t name) (m text))
 		(trace
-			(recv (enc m y x t (privk "pcs" y)) (enc m x y t (privk "sign" x))) ; ttp-resolve-request
-			(send (enc (enc m x y "abort" (privk x)) (privk t)))                ; ttp-resolve-response-aborted
+			(recv (enc (cat (enc m y x t (privk "pcs" y)) (enc m x y t (privk "sign" x))) (privk "ttp-sig" x))) ; ttp-resolve-request
+			(send (enc (enc (enc m x y "abort" (privk x)) (privk "sign" t)) (pubk "enc" x)))                    ; ttp-resolve-response-aborted
 		)
 	)
 
+)
+
+
+
+; ### preskeletons
+
+(defpreskeleton two-party-ocs
+	(vars (a b t name) (m text))
+	(defstrand init-main 4 (a a) (b b) (t t) (m m))
+	(non-orig
+		(privk "ttp-sig" a) (privk "enc" a) (privk "sign" a) (privk "pcs" a)
+		(privk "ttp-sig" b) (privk "enc" b) (privk "sign" b) (privk "pcs" b)
+		(privk "sign" t)
+	)
+	(uniq-orig m)
 )
